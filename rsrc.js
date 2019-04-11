10929fa67377f41a3e0ede396dc9f956ce601cbf
Nathan Kallman <nathan@joinroot.com>
2019-04-11 10:48:28 -0400

    Create rsrc methods and hello world example

--- rsrc.js
+++ rsrc.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('template[rsrc]').forEach((template) => {
-    const rsrcName = template.attributes.rsrc.value;
+  document.querySelectorAll('template[rsrc][name]').forEach((template) => {
+    const rsrcName = template.getAttribute('name');
+    const rsrcContent = template.content.cloneNode(true)
 
     customElements.define(rsrcName,
       class extends HTMLElement {
         constructor() {
           super();
-
           this.attachShadow({ mode: 'open' })
-            .appendChild(template.content.cloneNode(true));
+              .appendChild(rsrcContent);
+        }
       }
-    });
+    );
   });
 });
